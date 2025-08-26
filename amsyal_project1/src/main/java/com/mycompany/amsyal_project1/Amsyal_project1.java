2c01bd6fb4e81ac115490029207b61bf054303aa
Muhammad Amsyal Ikhsan <101682251+Amsyal@users.noreply.github.com>
2025-08-26 17:21:52 +0700

    pertemuan ke 1

---
+++ amsyal_project1/src/main/java/com/mycompany/amsyal_project1/Amsyal_project1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.amsyal_project1;
+
+/**
+ *
+ * @author acer
+ */
+public class Amsyal_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Muhammad Amsyal Ikhsan");
+        System.out.println("NPM: 2440506062");
+        System.out.println("Alamat: Demak City");
+        System.out.println("No_HP: 089524201070");
+
+
+
+    }
+}
